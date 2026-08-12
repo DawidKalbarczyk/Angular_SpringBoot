4647856318b7429c1a8120c1578a3704878bec90
dawid <u56128@student.wat.edu.pl>
2026-08-13 01:48:44 +0200

    wip: added return component to home component

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthorBar } from '../global-components/author-bar/author-bar';
 import {LoginCorner} from '../global-components/login-corner/login-corner';
+import { ReturnCorner } from '../global-components/return-corner/return-corner';
 
 @Component({
   selector: 'app-home',
-  imports: [AuthorBar, LoginCorner],
+  imports: [AuthorBar, LoginCorner, ReturnCorner],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
