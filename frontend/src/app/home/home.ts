3825298218dd347f41d0538b64255de4c4df1248
dawid <u56128@student.wat.edu.pl>
2026-08-14 14:20:22 +0200

    feat: finished working on home page. Added routes, styling and html

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AuthorBar } from '../global-components/author-bar/author-bar';
 import {LoginCorner} from '../global-components/login-corner/login-corner';
 import { ReturnCorner } from '../global-components/return-corner/return-corner';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [AuthorBar, LoginCorner, ReturnCorner],
+  imports: [AuthorBar, LoginCorner, ReturnCorner, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
