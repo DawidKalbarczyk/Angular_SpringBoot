62111a6ceae2228e5058bcda0c480bdf0fecd4f8
dawid <u56128@student.wat.edu.pl>
2026-08-14 16:18:01 +0200

    change: switched home router to be static not lazy

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./home/home').then(m => m.Home),
+        component: Home
     },
     {
         path: 'user',
