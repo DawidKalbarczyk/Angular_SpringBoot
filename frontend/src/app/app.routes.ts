c4fa81326367007f8beb1b06eeb1acdff00b6cb5
dawid <u56128@student.wat.edu.pl>
2026-08-14 13:56:15 +0200

    change: changed app to be as router-outlet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./home/home').then(m => m.Home),
+    },
+    {
+        path: 'geoportal',
+        loadComponent: () => import('./home/geoportal/geoportal').then(m => m.Geoportal)
+    },
+    {
+        path: 'history',
+        loadComponent: () => import('./home/history/history').then(m => m.History),
+    },
+    {
+        path: 'search',
+        loadComponent: () => import('./home/search/search').then(m => m.Search),
+    }
+];
