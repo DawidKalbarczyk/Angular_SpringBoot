ff4ed9897f86b28f438a02deb9e082406ae5eaaf
dawid <u56128@student.wat.edu.pl>
2026-08-14 16:19:56 +0200

    feat: added half of styling to user

--- frontend/src/app/user/user.ts
+++ frontend/src/app/user/user.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthorBar } from '../global-components/author-bar/author-bar';
+import { LoginCorner } from '../global-components/login-corner/login-corner';
+import { ReturnCorner } from '../global-components/return-corner/return-corner';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [AuthorBar, LoginCorner, ReturnCorner],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
