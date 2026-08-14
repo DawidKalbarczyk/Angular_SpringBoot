cf06e456fbca3caee60870c8f440d2f796781469
dawid <u56128@student.wat.edu.pl>
2026-08-14 14:20:47 +0200

    feat: generated geoportal component

---
+++ frontend/src/app/home/geoportal/geoportal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geoportal',
+  imports: [],
+  templateUrl: './geoportal.html',
+  styleUrl: './geoportal.scss',
+})
+export class Geoportal {}
