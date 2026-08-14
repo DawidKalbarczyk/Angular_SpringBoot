7591ce60d833144e97d8acb05a7bc1e73fc2bd32
dawid <u56128@student.wat.edu.pl>
2026-08-14 14:18:11 +0200

    feat: added hovering effects and routerLink to user container in login-corner

--- frontend/src/app/global-components/login-corner/login-corner.ts
+++ frontend/src/app/global-components/login-corner/login-corner.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-corner',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-corner.html',
   styleUrl: './login-corner.scss',
 })
