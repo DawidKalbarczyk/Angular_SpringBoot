a4f2b3dbe59ab7f9fe7044b5122cccac63e9b258
dawid <u56128@student.wat.edu.pl>
2026-08-17 17:41:16 +0200

    change: changed clickable events from profile-picture to Settings button feat: added redirecting if user isn't logged in and tries to enter the page

--- frontend/src/app/user/user.ts
+++ frontend/src/app/user/user.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AuthorBar } from '../global-components/author-bar/author-bar';
 import { LoginCorner } from '../global-components/login-corner/login-corner';
 import { ReturnCorner } from '../global-components/return-corner/return-corner';
+import { Router } from '@angular/router';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-user',
-  imports: [AuthorBar, LoginCorner, ReturnCorner],
+  standalone: true,
+  imports: [AuthorBar, LoginCorner, ReturnCorner, MatProgressSpinnerModule],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class User {}
+export class User {
+  public isLoggedIn: boolean = true;
+  private router: Router = inject(Router);
+  public url: string = this.router.url;
+  public redirectUrl: boolean = false;
+  constructor() {
+    if (this.isLoggedIn === true && this.router.url === '/user') {
+      return;
+    } else if (this.isLoggedIn === false && this.router.url === '/user') {
+      this.redirectUrl = true;
+      setTimeout(() => {
+        this.router.navigate(['/']);
+      }, 3000);
+    }
+  }
+}
