7e65b8bfd325a30e857bf8af1fdabe77e5a00bf4
dawid <u56128@student.wat.edu.pl>
2026-08-17 17:39:50 +0200

    feat: added main components to geoportal site

--- frontend/src/app/home/geoportal/geoportal.ts
+++ frontend/src/app/home/geoportal/geoportal.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { GeoportalHeadbar } from './geoportal-headbar/geoportal-headbar';
+import { ReturnCorner } from "../../global-components/return-corner/return-corner";
+import { MapComponent } from "./map-component/map-component";
 
 @Component({
   selector: 'app-geoportal',
-  imports: [],
+  imports: [GeoportalHeadbar, ReturnCorner, MapComponent],
   templateUrl: './geoportal.html',
   styleUrl: './geoportal.scss',
 })
