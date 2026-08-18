00404269d9a7f8ece57683b831e6a0017c459956
dawid <u56128@student.wat.edu.pl>
2026-08-18 13:55:16 +0200

    feat: added author-bar to geoportal page

--- frontend/src/app/home/geoportal/geoportal.ts
+++ frontend/src/app/home/geoportal/geoportal.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { GeoportalHeadbar } from './geoportal-headbar/geoportal-headbar';
 import { ReturnCorner } from "../../global-components/return-corner/return-corner";
 import { MapComponent } from "./map-component/map-component";
+import { AuthorBar } from "../../global-components/author-bar/author-bar";
 
 @Component({
   selector: 'app-geoportal',
-  imports: [GeoportalHeadbar, ReturnCorner, MapComponent],
+  imports: [GeoportalHeadbar, ReturnCorner, MapComponent, AuthorBar],
   templateUrl: './geoportal.html',
   styleUrl: './geoportal.scss',
 })
