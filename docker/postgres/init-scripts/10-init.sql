475d6730e39c3b585a70962defcb6ad34b5d893b
dawid <u56128@student.wat.edu.pl>
2026-09-10 16:23:57 +0200

    feat: added craeating db if doesnt exist

--- docker/postgres/init-scripts/10-init.sql
+++ docker/postgres/init-scripts/10-init.sql
@@ -1,2 +1,9 @@
 create extension postgis;
 create extension pgrouting;
+
+create table if not exists users (
+	user_id varchar(128) primary key,
+	user_name varchar(255) not null,
+	user_password varchar(255),
+	photo_url text
+);
