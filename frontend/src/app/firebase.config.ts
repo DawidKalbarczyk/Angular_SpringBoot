3ba1aa2508a03cbfecc096ec3e39e8c55402db18
dawid <u56128@student.wat.edu.pl>
2026-09-10 11:46:49 +0200

    feat: added Firebase authentication

---
+++ frontend/src/app/firebase.config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBuswFV7M1gfjHsx59k8O9kNfbMb4RKfg4',
+    authDomain: 'angular-85978.firebaseapp.com',
+    projectId: 'angular-85978',
+    storageBucket: 'angular-85978.firebasestorage.app',
+    messagingSenderId: '1023744121884',
+    appId: '1:1023744121884:web:e073b98b9fbe7c817713d9',
+    measurementId: 'G-N4RNMDHHNL'
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
