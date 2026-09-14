dfa056c29f3500e2e1270faa77cc8cc2610caa41
dawid <u56128@student.wat.edu.pl>
2026-09-14 10:04:40 +0200

    fix: repaired wrong atribute names in table creation query

--- docker/postgres/init-scripts/10-init.sql
+++ docker/postgres/init-scripts/10-init.sql
@@ -2,8 +2,8 @@ create extension postgis;
 create extension pgrouting;
 
 create table if not exists users (
-	user_id varchar(128) primary key,
-	user_name varchar(255) not null,
-	user_password varchar(255),
-	photo_url text
+	id varchar(128) primary key,
+	username varchar(255) not null,
+	email varchar(255),
+	photoUrl text
 );
